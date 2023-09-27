d14c75c2d72a643a4f9a9f41b6947e10d94786d3
cmr927 <142535038+cmr927@users.noreply.github.com>
2023-09-27 18:00:13 -0400

    Made pokemonList & added details in scripts.js
    Made pokemonList & added details in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+let pokemonList = []
+pokemonList = [
+    {
+        name: 'Geodude',
+        height: 0.4,
+        types: ['Rock', 'Ground'],
+        abilities: ['Sturdy', 'Sand-veil', 'Rock-head']
+    },
+
+    {
+        name: 'Graveler',
+        height: 1,
+        types: ['rock', 'ground'],
+        abilities: ['Sturdy', 'Sand-veil', 'Rock-head'],
+    },
+
+    {
+        name: 'Golem',
+        height: 1.4,
+        types: ['rock', 'ground'],
+        abilities: ['Sturdy', 'Sand-veil', 'Rock-head'],
+    },
+];
