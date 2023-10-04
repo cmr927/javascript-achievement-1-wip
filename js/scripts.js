f365afa0aeabcc36a754c32547097d5b7a18060b
cmr927 <142535038+cmr927@users.noreply.github.com>
2023-10-03 23:50:02 -0400

    Made for loop for pokemon & styled

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,23 @@ pokemonList = [
     {
         name: 'Graveler',
         height: 1,
-        types: ['rock', 'ground'],
+        types: ['Rock', 'Ground'],
         abilities: ['Sturdy', 'Sand-veil', 'Rock-head'],
     },
 
     {
         name: 'Golem',
         height: 1.4,
-        types: ['rock', 'ground'],
+        types: ['Rock', 'Ground'],
         abilities: ['Sturdy', 'Sand-veil', 'Rock-head'],
     },
+
 ];
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.3) { //if pokemon's height is over 1.4 meters also print 'Wow that's big!'
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' meters) - Wow, that\'s big! <br>');
+    }
+    else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' meters) <br>');
+    }
+}
