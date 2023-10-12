fea0d86631ff02027cac7b7d56348f8ce35fd44d
cmr927 <142535038+cmr927@users.noreply.github.com>
2023-10-12 14:58:16 -0400

    Changed to =>

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ pokemonList = [
     },
 
 ];
-pokemonList.forEach(function(pokemonList){
+pokemonList.forEach((pokemonList) => {
     if (pokemonList.height > 1.3) { //if pokemon's height is over 1.4 meters also print 'Wow that's big!'
         document.write(pokemonList.name + ' (height: ' + pokemonList.height + ' meters) - Wow, that\'s big! <br>');
     }
